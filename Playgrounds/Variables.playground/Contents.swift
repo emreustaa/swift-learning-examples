75f1a4ba807718e84f4d18e70889fc8272c3439f
Emre USTA <emre.usta@stu.fsm.edu.tr>
2022-04-10 01:13:58 +0300

    Works

---
+++ Playgrounds/Variables.playground/Contents.swift
@@ -0,0 +1,62 @@
+import UIKit
+
+
+// -------------- PART 1 --------------
+//Variables & Constants
+
+// snake case -> var user_name = "James"
+
+// camelCase -> var userName -> swift default olarak bu yazım şeklini kullanır.
+
+// String
+
+var userName = "James"
+
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Hetfield"
+
+userName = "Lars"
+
+// integer & double & float
+
+// integer
+let userAge = 50
+let myNumber = 4
+
+userAge / myNumber
+
+// double
+let userAgeD = 50.0
+var myNumberD = 4.0
+
+userAgeD / myNumberD
+
+let pi = 3.14
+
+// boolean
+
+var myBoolean = false
+
+myBoolean = true
+
+myNumberD = 3.0
+
+
+// -------------- PART 2 --------------
+
+let myString : String = "50"
+let anotherNumber : Int = 10
+
+let stringNumber : String = String(20)
+
+let myVariable : String
+
+// initialization
+
+myVariable = "Emre"
+let myUpperVariable = myVariable.uppercased()
+print(myUpperVariable)
+print(myVariable)
