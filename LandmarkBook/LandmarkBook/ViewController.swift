8b7dabedbf790ec96d4f99f1835557b1ea564064
Emre USTA <emre.usta@stu.fsm.edu.tr>
2022-04-10 02:22:15 +0300

    landmark app continue

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var talbeView: UITableView!
+    
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        talbeView.delegate = self
+        talbeView.dataSource = self
+        
+        // Landmark Book Data
+        
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Taj Mahal")
+        
+        
+        landmarkImages.append( UIImage(named: "colosseum.jpg")!)
+        landmarkImages.append( UIImage(named: "greatwall.jpg")!)
+        landmarkImages.append( UIImage(named: "kremlin.jpg")!)
+        landmarkImages.append( UIImage(named: "stonehenge.jpg")!)
+        landmarkImages.append( UIImage(named: "tajmahal.jpg")!)
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landmarkNames[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNames.count
+    }
+    
 }
 
